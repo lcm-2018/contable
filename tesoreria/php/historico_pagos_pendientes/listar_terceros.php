9a58158badcf106f5e39cf701a5119c4532ee74c
jesusbr7 <jesusbr7chucho@gmail.com>
2025-04-15 17:53:21 -0500

    modificacion informe supersalud

--- tesoreria/php/historico_pagos_pendientes/listar_terceros.php
+++ tesoreria/php/historico_pagos_pendientes/listar_terceros.php
@@ -124,6 +124,7 @@ try {
                     INNER JOIN tb_terceros ON (ctb_libaux.id_tercero_api = tb_terceros.id_tercero_api)
                 WHERE ctb_doc.id_tipo_doc = 3
                     AND DATE_FORMAT(ctb_libaux.fecha_reg, '%Y-%m-%d') <= '$fecha'
+                    AND ctb_libaux.ref = 1
                 GROUP BY 
                     ctb_libaux.id_ctb_doc, tb_terceros.id_tercero_api
                 ) c
