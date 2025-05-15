00170a34beab40d879787ffc90c851e38da1e99f
jesusbr7 <jesusbr7chucho@gmail.com>
2025-05-15 17:48:34 -0500

    libros auxiliares de presupuesto

--- presupuesto/js/libros_aux_pto/libros_aux_pto.js
+++ presupuesto/js/libros_aux_pto/libros_aux_pto.js
@@ -1,20 +1,32 @@
 (function ($) {
-    /*$('#frm_libros_aux_bancos').on("click", "#btn_consultar", function () {
-        $.post(window.urlin + '/tesoreria/php/informes_bancos/imp_libros_bancos.php', {
-            id_cuenta_ini: $('#id_txt_cuentainicial').val(),
-            id_cuenta_fin: $('#id_txt_cuentafinal').val(),
-            fec_ini: $('#txt_fecini').val(),
-            fec_fin: $('#txt_fecfin').val(),
-            id_tipo_doc: $('#sl_tipo_documento').val(),
-            id_tercero: $('#id_txt_tercero').val()
-        }, function (he) {
-            $('#divTamModalImp').removeClass('modal-sm');
-            $('#divTamModalImp').removeClass('modal-lg');
-            $('#divTamModalImp').addClass('modal-xl');
-            $('#divModalImp').modal('show');
-            $("#divImp").html(he);
-        });
-    });*/
+    $('#frm_libros_aux_presupuesto').on("click", "#btn_consultar", function () {        
+        var id_cargue = 0;
+        id_cargue = $('#id_cargue').val();
+        var doc_fuente = 0;
+        doc_fuente = $('#sl_doc_fuente').val();
+        if (id_cargue == 0) {
+            mjeError("Seleccione un tipo de documento");
+        }
+        else {
+            if (doc_fuente != 1) {
+                mjeError("Seleccione documento fuente CDP");
+            }
+            else {
+                $.post(window.urlin + '/presupuesto/php/libros_aux_pto/imp_libros_aux_pto.php', {
+                    id_cargue: id_cargue,
+                    doc_fuente: doc_fuente,
+                    fec_ini: $('#txt_fecini').val(),
+                    fec_fin: $('#txt_fecfin').val(),
+                }, function (he) {
+                    $('#divTamModalImp').removeClass('modal-sm');
+                    $('#divTamModalImp').removeClass('modal-lg');
+                    $('#divTamModalImp').addClass('modal-xl');
+                    $('#divModalImp').modal('show');
+                    $("#divImp").html(he);
+                });
+            }
+        }
+    });
 })(jQuery);
 
 //buscar con 2 letras tipo documento
